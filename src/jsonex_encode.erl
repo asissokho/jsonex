82e65962915aecca5e0d2dfcf923d1232833a175
asissokho <abdoulaye.sissokho@hotmail.fr>
2017-12-31 18:54:39 +0100

    matching order

--- src/jsonex_encode.erl
+++ src/jsonex_encode.erl
@@ -135,8 +135,8 @@ value({{json_utf8, T}}, Nexts, Buf, Opt) ->
     end;
 value(Value, Nexts, Buf, Opt) when is_integer(Value) -> next(Nexts, <<Buf/binary, (integer_to_binary(Value))/binary>>, Opt);
 value(Value, Nexts, Buf, Opt) when is_float(Value)   -> next(Nexts, <<Buf/binary, (float_to_binary(Value, Opt?OPT.float_format))/binary>>, Opt);
-value(Value, Nexts, Buf, Opt) when ?IS_STR(Value)    -> string(Value, Nexts, Buf, Opt);
 value(Value, Nexts, Buf, Opt= ?OPT{next_as_str = 1}) -> string(Value, Nexts, Buf, Opt?OPT{next_as_str = 0});
+value(Value, Nexts, Buf, Opt) when ?IS_STR(Value)    -> string(Value, Nexts, Buf, Opt);
 value({{_,_,_},{_,_,_}} = Value, Nexts, Buf, Opt)    -> datetime(Value, Nexts, Buf, Opt);
 value({Value}, Nexts, Buf, Opt)                      -> object(Value, Nexts, Buf, Opt);
 value([{}], Nexts, Buf, Opt)                         -> object([], Nexts, Buf, Opt);
